d6e04fe6747357f23804ed9ff28bc57e4dd0ad08
Dinesh Chetty <dinesh@trackitportal.com>
2025-08-28 15:19:38 +1200

    Added nuget packages for InMemoryDatabase and configured FlightDBContext to Program.cs file in API project

--- FlightInformationApi/Program.cs
+++ FlightInformationApi/Program.cs
@@ -1,3 +1,6 @@
+using FlightInformationApi.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Entity Framework
+builder.Services.AddDbContext<FlightDbContext>(options =>
+    options.UseInMemoryDatabase("FlightDatabase"));
 
 var app = builder.Build();
 
